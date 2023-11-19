4b94979223c7bf95abd8731e609f0d1d1de3e781
ArvidErik <erik.arvid.hosszu@gmail.com>
2023-11-19 19:11:18 +0100

    Transaction grid improved

--- src/pages/transactions/index.jsx
+++ src/pages/transactions/index.jsx
@@ -1,8 +1,50 @@
-import React from 'react'
+import { ThemeContext, useTheme } from '@emotion/react'
+import { Box, Typography } from '@mui/material'
+import { DataGrid } from '@mui/x-data-grid'
+import React, {useState} from 'react'
+
+
+
 
 function Transactions() {
+  const theme = useTheme();
+
   return (
-    <div>Transactions</div>
+    <Box m="1.5rem 2.5rem">
+      <Typography variant='h4'>Incomes</Typography>
+      <Box height="80vh"
+      sx={{
+        "& .MuiDataGrid-root":{
+          border: "none"
+        },
+        "& .MuiDataGrid-cell":{
+          borderBottom: "none",
+        },
+        "& .MuiDataGrid-columnHeaders": {
+          backgroundColor: theme.palette.background.alt,
+          color: theme.palette.secondary[100],
+          borderBottom: "none",
+        },
+        "& .MuiDataGrid-virtualScroller": {
+          backgroundColor: theme.palette.primary.light,
+        },
+        "& .MuiDataGrid-footerContainer": {
+            backgroundColor: theme.palette.background.alt,
+            color: theme.palette.secondary[100],
+            borderTop: "none",
+        },
+        "& .MuiDataGrid-toolbarContainer .MuiButton-text": {
+          color: `${theme.palette.secondary[200]} !important`
+        }
+
+      }}
+      >
+        <DataGrid
+          getRowId={(row) => row._id}
+        />
+      </Box>
+
+    </Box>
   )
 }
 
